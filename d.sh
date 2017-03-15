5383784de8dd32d6c27922267f59c2d50f7d4452
dht <info@drhaitao.cn>
2017-03-15 15:13:02 +0000

    update

--- d.sh
+++ d.sh
@@ -1,4 +1,5 @@
-cp -R /home/daren/src/drhaitao/public/* /home/drhaitao/web/
+cp -R /home/frank/src/web/drhaitao/public/* /home/drhaitao/web/
 git add --all
 git commit -m "update"
 git push origin master
+
